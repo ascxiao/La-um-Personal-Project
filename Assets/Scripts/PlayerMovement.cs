7177606767a03fd2061f212fdd1288211b055a09
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2025-11-08 00:27:14 +0800

    Omni walking!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,6 +9,15 @@ public class PlayerMovement : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    public SpriteRenderer spriteRenderer;
+    public List<Sprite> nSprites;
+    public List<Sprite> neSprites;
+    public List<Sprite> eSprites;
+    public List<Sprite> seSprites;
+    public List<Sprite> sSprites;
+
+    public float frameRate;
+    float idleTime;
 
     private void Awake(){
         playerControls = new PlayerControls();
@@ -26,10 +37,53 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void PlayerInput(){
-        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        movement = playerControls.Movement.Move.ReadValue<Vector2>().normalized;
     }
 
     private void Move(){
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+        FlipHandling();
+        List <Sprite> directionSprites = GetSpriteDirection();
+        
+        if (directionSprites != null){
+            float playTime = Time.time - idleTime;
+            int frame = (int)((playTime * frameRate)%directionSprites.Count);
+            spriteRenderer.sprite = directionSprites[frame];
+        }else{
+            idleTime = Time.time;
+        }
+    }
+
+    void FlipHandling(){
+        if(!spriteRenderer.flipX&&movement.x < 0){
+            spriteRenderer.flipX = true;
+        } else if (spriteRenderer.flipX && movement.x > 0){
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    List<Sprite> GetSpriteDirection(){
+
+        List<Sprite> selectedSprites = null;
+        if(movement.y > 0){ //North
+            if (Mathf.Abs(movement.x)>0){
+                selectedSprites = neSprites;
+            } else {
+                selectedSprites = nSprites;
+            }
+
+        }else if(movement.y < 0){ //South
+            if (Mathf.Abs(movement.x)>0){
+                selectedSprites = seSprites;
+            } else {
+                selectedSprites = sSprites;
+            }
+        }else{
+            if (Mathf.Abs(movement.x)>0){
+                selectedSprites = eSprites;
+            }
+        }
+
+        return selectedSprites;
     }
 }
