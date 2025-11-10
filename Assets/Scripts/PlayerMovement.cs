930ad941d3f595855919ad7d0591a2b37904cd75
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2025-11-10 12:10:19 +0800

    established animator!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,7 @@ public class PlayerMovement : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
-    public SpriteRenderer spriteRenderer;
-    public List<Sprite> nSprites;
-    public List<Sprite> neSprites;
-    public List<Sprite> eSprites;
-    public List<Sprite> seSprites;
-    public List<Sprite> sSprites;
+    private Animator animator;
 
     public float frameRate;
     float idleTime;
@@ -22,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake(){
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable(){
@@ -42,51 +38,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move(){
         rb.linearVelocity = movement * moveSpeed;
-        if (movement == Vector2.zero)
+        if (movement == Vector2.zero){
             rb.linearVelocity = Vector2.zero;
-
-        FlipHandling();
-        List <Sprite> directionSprites = GetSpriteDirection();
-        
-        if (directionSprites != null){
-            float playTime = Time.time - idleTime;
-            int frame = (int)((playTime * frameRate)%directionSprites.Count);
-            spriteRenderer.sprite = directionSprites[frame];
         }else{
-            idleTime = Time.time;
+            animator.SetFloat("XInput", movement.x);
+            animator.SetFloat("YInput", movement.y);
         }
-    }
-
-    void FlipHandling(){
-        if(!spriteRenderer.flipX&&movement.x < 0){
-            spriteRenderer.flipX = true;
-        } else if (spriteRenderer.flipX && movement.x > 0){
-            spriteRenderer.flipX = false;
-        }
-    }
-
-    List<Sprite> GetSpriteDirection(){
-
-        List<Sprite> selectedSprites = null;
-        if(movement.y > 0){ //North
-            if (Mathf.Abs(movement.x)>0){
-                selectedSprites = neSprites;
-            } else {
-                selectedSprites = nSprites;
-            }
-
-        }else if(movement.y < 0){ //South
-            if (Mathf.Abs(movement.x)>0){
-                selectedSprites = seSprites;
-            } else {
-                selectedSprites = sSprites;
-            }
-        }else{
-            if (Mathf.Abs(movement.x)>0){
-                selectedSprites = eSprites;
-            }
-        }
-
-        return selectedSprites;
     }
 }
