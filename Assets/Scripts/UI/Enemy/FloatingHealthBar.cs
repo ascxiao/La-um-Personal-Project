98ec7e60d83783b79c94e2c9b913bb90ab624a95
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2025-11-19 14:21:08 +0800

    Fixed floating health bar flip

--- Assets/Scripts/UI/Enemy/FloatingHealthBar.cs
+++ Assets/Scripts/UI/Enemy/FloatingHealthBar.cs
@@ -5,22 +5,6 @@ using System.Collections;
 public class FloatingHealthBar : MonoBehaviour
 {
     [SerializeField] private Slider slider;
-    private float lastParentXScale;
-    private void Update()
-    {
-        ////FIX THISSSSSSSSSSSS DOESNT WORK
-        if (transform.parent != null)
-        {
-            Transform parentTransform = transform.parent;
-            Vector3 parentPos = transform.parent.position;
-            float currentPXScale = parentTransform.localScale.x;
-            if (currentPXScale != lastParentXScale)
-            {
-                slider.transform.localScale = new Vector3(slider.transform.localScale.x * -1, slider.transform.localScale.y, slider.transform.localScale.z);
-                lastParentXScale = currentPXScale;
-            }
-        }
-    }
 
     public void UpdateHealthBar(float currentValue, float maxValue)
     {
